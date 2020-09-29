882ca5beda53351ba459230352153ae0cdb24ed2
Brian Breeden <breedenb@vt.edu>
2020-09-29 19:01:23 -0400

    refactor of the launch object.

--- launch.py
+++ launch.py
@@ -2,12 +2,28 @@ import requests
 import ciso8601
 import datetime
 
+class Launch:
+    def __init__(self):
+        data = launch_init()
+        self.mission = data['name']
+        self.mission_description = get_mission_description(data)
+        self.lsp = get_lsp(data)
+        self.lsp_abbrev = get_lsp_abbrev(data)
+        self.lsp_country = data['pad']['location']['country_code']
+        self.rocket_name = data['rocket']['configuration']['name']
+        self.rocket_info_url = data['rocket']['configuration']['wiki_url']
+        self.net = get_timedate_stamp(data['net']) #UTC
+        self.img_url = get_img_url(data['image'])
+        self.launch_time_date = datetime_to_string(data['net'])
+        self.lsp_info_url = data['launch_service_provider']['wiki_url']
+        self.launch_complex = data['pad']['location']['name']
+        self.launch_complex_info_url = data['pad']['wiki_url']
+        self.stream = get_stream_url(data)
+
 '''
 Makes the api call to launch library.
-
 args:
     None
-
 return:
     api call return as a json object
 '''
@@ -24,10 +40,8 @@ def launch_init():
     
 '''
 Returns the launch information url. If there is no information, None is returned.
-
 args:
     data - the JSON data from the api call.
-
 return:
     None - If the length of the array is zero, it can be assumed there is no information for this launch
     data_array[0] - The first entry in the array is the primary data source for this metric
@@ -40,10 +54,8 @@ def get_launch_info_url(data):
 
 '''
 Returns the video stream url. If there is no information, None is returned.
-
 args:
     data - the JSON data from the api call.
-
 return:
     None - If the length of the array is zero, it can be assumed there is no stream for this launch
     data_array[0] - The first entry in the array is the primary data source for this metric
@@ -53,12 +65,11 @@ def get_stream_url(data):
     if len(data_array) == 0:
         return None
     return data_array[0]['url']
+
 '''
 Returns a formatted string that is used by the countdown script.
-
 args:
     date_string - the time and date information in the form of a string
-
 return:
     Re-formatted string of the date_string.
 '''
@@ -66,12 +77,11 @@ def get_timedate_stamp(date_string):
     dt = ciso8601.parse_datetime(date_string)
     dt_st = ('%s, %s, %s, %s, %s, %s'% (dt.year, (dt.month-1), dt.day, dt.hour, dt.minute, dt.second))
     return dt_st
+
 '''
 Takes a date string from the API and returns a re-formatted string that is easier to read for the UI.
-
 args:
     date_string - the time and date information in the form of a string
-
 return:
     A reformatted date string.
 '''
@@ -81,10 +91,8 @@ def datetime_to_string(date_string):
 
 '''
 Takes the img url from the api as an argument and returns our own default image if the placeholder from the api is used. Otherwise, use the passed url.
-
 args:
     api_string - the url string from the api
-
 return:
     string - custom placeholder or the appropriate img url
 '''
@@ -94,41 +102,41 @@ def get_img_url(api_string):
     api_string = api_string.replace('\\', '')
     return api_string
 
+'''
+args:
+    data - the JSON data from the api call.
+return:
+    string - mission description present in the api call, or
+    None - if a mission description is not present
+'''
+def get_mission_description(data):
+    try:
+        return data['mission']['description']
+    except TypeError:
+        return None
 
+'''
+args:
+    data - the JSON data from the api call.
+return:
+    string - the LSP for the launch object, or
+    string - "LSP Unknown" if a LSP is not present
+'''
+def get_lsp(data):
+    try:
+        return data['launch_service_provider']['name']
+    except TypeError:
+        return "LSP Unknown"
 
-
-class Launch:
-    def __init__(self):
-        data = launch_init()
-        self.mission = data['name']
-
-        # Attempt tp fetch a mission description
-        try:
-            self.mission_description = data['mission']['description']
-        except TypeError:
-            self.mission_description = None
-            
-        # Attempt to fetch a LSP.
-        try:
-            self.lsp = data['launch_service_provider']['name']
-        except TypeError:
-            self.lsp = "LSP Unknown"
-            print('LSP unknown for:', self.mission)
-
-        # Attempt to fetch a LSP Abbrev.
-        try:
-            self.lsp_abbrev = data['launch_service_provider']['abbrev']
-        except TypeError:
-            self.lsp_abbrev = "UKN"
-            print('LSP abbrev unknown for:', self.mission)
-        
-        self.lsp_country = data['pad']['location']['country_code']
-        self.rocket_name = data['rocket']['configuration']['name']
-        self.rocket_info_url = data['rocket']['configuration']['wiki_url']
-        self.net = get_timedate_stamp(data['net']) #UTC
-        self.img_url = get_img_url(data['image'])
-        self.launch_time_date = datetime_to_string(data['net'])
-        self.lsp_info_url = data['launch_service_provider']['wiki_url']
-        self.launch_complex = data['pad']['location']['name']
-        self.launch_complex_info_url = data['pad']['wiki_url']
-        self.stream = get_stream_url(data)
+'''
+args:
+    data - the JSON data from the api call.
+return:
+    string - the LSP abbreviation for the launch object, or
+    string - "UKN" if a LSP abbreviation is not present
+'''
+def get_lsp_abbrev(data):
+    try:
+        return data['launch_service_provider']['abbrev']
+    except TypeError:
+        return "UKN"
